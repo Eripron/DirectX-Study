7b27025eb08c068ea139a6c3384ad6cf315cff66
Jin Hyeok <jinhyeok.heo@bagelcode.com>
2025-05-19 21:11:01 +0900

    Window 프로젝트 생성

---
+++ DirectX_Study/DirectX_Study/Main.cpp
@@ -0,0 +1,61 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("Graphic");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstace, LPSTR lpszCmdParam, int nCmdShow)
+{
+	g_hInst = hInstance;
+
+	MSG Message;
+	WNDCLASS WndClass;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(RGB(0, 0, 0));
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	int x = GetSystemMetrics(SM_CXFULLSCREEN) / 4;
+	int y = GetSystemMetrics(SM_CYFULLSCREEN) / 4;
+	HWND hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, x, y, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+
+	switch (iMessage)
+	{
+	case WM_CREATE:
+		return 0;
+
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return (DefWindowProc(hWnd, iMessage, wParam, lParam));;
+}
