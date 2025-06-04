c9a539e2ee0f95ed16594a7bb8293bcf7758ee92
Jin Hyeok <jinhyeok.heo@bagelcode.com>
2025-06-04 15:29:24 +0900

    * Vector2/3 연산자 및 함수 추가

--- DirectX_Study/DirectX_Study/Numeric.cpp
+++ DirectX_Study/DirectX_Study/Numeric.cpp
@@ -2,36 +2,236 @@
 
 namespace Numeric
 {
+#pragma region Vector2
+
 	// vector2
 	Vector2::Vector2(float _x, float _y) : x(_x), y(_y)
 	{
 	}
+
+	Vector2 Vector2::Zero()
+	{
+		return Vector2(0, 0);
+	}
+
+	Vector2 Vector2::One()
+	{
+		return Vector2(1, 1);
+	}
+
+	float Vector2::Distance(const Vector2& _a, const Vector2& _b)
+	{ 
+		return sqrtf(DistanceSquared(_a, _b));
+	}
+
+	float Vector2::DistanceSquared(const Vector2& _a, const Vector2& _b)
+	{
+		float dx = _b.x - _a.x;
+		float dy = _b.y - _a.y;
+
+		return (dx * dx) + (dy * dy);
+	}
+
+	float Vector2::Magnitude()
+	{
+		return Distance(Vector2(0, 0), *this);
+	}
+
+	Vector2 Vector2::Normalize()
+	{
+		return Vector2(x, y) / Magnitude();
+	}
+
+#pragma region operator
 
 	Vector2 Vector2::operator+(const Vector2& _other) const
 	{
 		return Vector2(x + _other.x, y + _other.y);
 	}
 
-	Vector2 operator+(const Vector2& _a, const Vector2& _b)
+	Vector2& Vector2::operator+=(const Vector2& _other)
 	{
-		return Vector2(_a.x + _b.x, _a.y + _b.y);
+		x += _other.x;
+		y += _other.y;
+
+		return *this;
 	}
 
+	Vector2 Vector2::operator-(const Vector2& _other) const
+	{
+		return Vector2(x - _other.x, y - _other.y);
+	}
 
+	Vector2& Vector2::operator-=(const Vector2& _other)
+	{
+		x -= _other.x;
+		y -= _other.y;
+
+		return *this;
+	}
+
+	Vector2 Vector2::operator*(float _value) const
+	{
+		return Vector2(x * _value, y * _value);
+	}
+
+	Vector2& Vector2::operator*=(float _value)
+	{
+		x *= _value;
+		y *= _value;
+
+		return *this;
+	}
+
+	Vector2 Vector2::operator/(float _value) const
+	{
+		return Vector2(x / _value, y / _value);
+	}
+
+	Vector2& Vector2::operator/=(float _value)
+	{
+		x /= _value;
+		y /= _value;
+
+		return *this;
+	}
+
+	bool Vector2::operator==(const Vector2& _other) const
+	{
+		return x == _other.x && y == _other.y;
+	}
+
+	bool Vector2::operator!=(const Vector2& _other) const
+	{
+		return x != _other.x || y != _other.y;
+	}
+
+	Vector2 operator*(float _value, const Vector2 _other)
+	{
+		return _other * _value;
+	}
+
+#pragma endregion
+
+#pragma endregion
+
+
+#pragma region Vector3
 
 	// vector3
 	Vector3::Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z)
 	{
 	}
+	
+	Vector3 Vector3::Zero()
+	{
+		return Vector3(0, 0, 0);
+	}
+
+	Vector3 Vector3::One()
+	{
+		return Vector3(1, 1, 1);
+	}
+
+	float Vector3::Distance(const Vector3& _a, const Vector3& _b)
+	{
+		return sqrtf(DistanceSquared(_a, _b));
+	}
+
+	float Vector3::DistanceSquared(const Vector3& _a, const Vector3& _b)
+	{
+		float dx = _b.x - _a.x;
+		float dy = _b.y - _a.y;
+		float dz = _b.z - _a.z;
+
+		return pow(dx, 2) + pow(dy, 2) + pow(dz, 2);
+	}
+
+	float Vector3::Magnitude()
+	{
+		return Distance(Vector3(0, 0, 0), *this);
+	}
+
+	Vector3 Vector3::Normalize()
+	{
+		return Vector3(x, y, z) / Magnitude();
+	}
+
+#pragma region operator
 
 	Vector3 Vector3::operator+(const Vector3& _other) const
 	{
 		return Vector3(x + _other.x, y + _other.y, z + _other.z);
 	}
 
-	Vector3 operator+(const Vector3& _a, const Vector3& _b)
+	Vector3& Vector3::operator+=(const Vector3& _other)
 	{
-		return Vector3(_a.x + _b.x, _a.y + _b.y, _a.z + _b.z);
+		x += _other.x;
+		y += _other.y;
+		z += _other.z;
+
+		return *this;
 	}
+
+	Vector3 Vector3::operator-(const Vector3& _other) const
+	{
+		return Vector3(x - _other.x, y - _other.y, z - _other.z);
+	}
+
+	Vector3& Vector3::operator-=(const Vector3& _other)
+	{
+		x -= _other.x;
+		y -= _other.y;
+		z -= _other.z;
+
+		return *this;
+	}
+
+	Vector3 Vector3::operator*(float _value) const
+	{
+		return Vector3(x * _value, y * _value, z * _value);
+	}
+
+	Vector3& Vector3::operator*=(float _value)
+	{
+		x *= _value;
+		y *= _value;
+		z *= _value;
+
+		return *this;
+	}
+
+	Vector3 Vector3::operator/(float _value) const
+	{
+		return Vector3(x / _value, y / _value, z / _value);
+	}
+
+	Vector3& Vector3::operator/=(float _value)
+	{
+		x /= _value;
+		y /= _value;
+		z /= _value;
+
+		return *this;
+	}
+
+	bool Vector3::operator==(const Vector3& _other) const
+	{
+		return (x == _other.x && y == _other.y && z == _other.z);
+	}
+
+	bool Vector3::operator!=(const Vector3& _other) const
+	{
+		return (x != _other.x || y != _other.y || z != _other.z);
+	}
+
+	Vector3 operator*(float _value, const Vector3 _other)
+	{
+		return _other * _value;
+	}
+
+#pragma endregion
+
+#pragma endregion
 
 }
--- DirectX_Study/DirectX_Study/Numeric.h
+++ DirectX_Study/DirectX_Study/Numeric.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <math.h>
+
 namespace Numeric
 {
+
+#pragma region Vector2
+
     struct Vector2
     {
         float x;
@@ -9,11 +14,39 @@ namespace Numeric
 
         Vector2(float _x = 0, float _y = 0);
 
+        static Vector2 Zero();
+        static Vector2 One();
+
+        static float Distance(const Vector2& _a, const Vector2& _b);
+        static float DistanceSquared(const Vector2& _a, const Vector2& _b);
+        float Magnitude();
+        Vector2 Normalize();
+
+        // addition vector
         Vector2 operator+(const Vector2& _other) const;
+        Vector2& operator+=(const Vector2& _other);
+
+        // subtraction vector
+        Vector2 operator-(const Vector2& _other) const;
+        Vector2& operator-=(const Vector2& _other);
+
+        // multiply vector
+        Vector2 operator*(float _value) const;
+        Vector2& operator*=(float _value);
+
+        // division vector
+        Vector2 operator/(float _value) const;
+        Vector2& operator/=(float _value);
+
+        // equality vector
+        bool operator==(const Vector2& _other) const;
+        bool operator!=(const Vector2& _other) const;
+
     };
 
-    Vector2 operator+(const Vector2& _a, const Vector2& _b);
+    Vector2 operator*(float _value, const Vector2 _other);
 
+#pragma endregion
 
 
     struct Vector3
@@ -24,9 +57,35 @@ namespace Numeric
 
 		Vector3(float _x = 0, float _y = 0, float _z = 0);
 
-        Vector3 operator+(const Vector3& _other) const;
-	};
+        static Vector3 Zero();
+        static Vector3 One();
 
-    Vector3 operator+(const Vector3& _a, const Vector3& _b);
+        static float Distance(const Vector3& _a, const Vector3& _b);
+        static float DistanceSquared(const Vector3& _a, const Vector3& _b);
+        float Magnitude();
+        Vector3 Normalize();
+
+        // addition vector
+        Vector3 operator+(const Vector3& _other) const;
+        Vector3& operator+=(const Vector3& _other);
+
+        // subtraction vector
+        Vector3 operator-(const Vector3& _other) const;
+        Vector3& operator-=(const Vector3& _other);
+
+        // multiply vector
+        Vector3 operator*(float _value) const;
+        Vector3& operator*=(float _value);
+
+        // division vector
+        Vector3 operator/(float _value) const;
+        Vector3& operator/=(float _value);
+
+        // equality vector
+        bool operator==(const Vector3& _other) const;
+        bool operator!=(const Vector3& _other) const;
+    };
+
+    Vector3 operator*(float _value, const Vector3 _other);
 
 }
